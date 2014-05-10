27db86d1d524d967af2ff8e58de1778ed1caa229
Jarred O. Green <jarredogreen@gmail.com>
2014-05-10 18:00:33 +1200

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :username, :presence = true, uniqiuness = true
+	validates :email, :presence = true, uniqueness = true;
+	validates_length_of :password, :in = 6..20, :on = :create
 end
