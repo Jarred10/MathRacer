ed4f8a058e9aa8874c0cc31a426dcb14f28b7ca6
Fayrr <penguin-commander@hotmail.com>
2014-05-16 14:41:50 +1200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 # Class defines a user, validates username, email, password and the length of password is between 6 t0 20 digits
 class User < ActiveRecord::Base
+	# Checks the username is provided and is unique
 	validates :username, :presence => true, :uniqueness => true
+	# Checks the email is provided and is unique
 	validates :email, :presence => true, :uniqueness => true;
+	# Checks that the two passwords provided match
 	validates_confirmation_of :password
+	# Checks the password is an acceptable length
 	validates_length_of :password, :in => 6..20, :on => :create
 	
 	# Authenticate a users log in attempt by comparing their username and password against the database
