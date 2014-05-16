2c57239bb7a3573d91718e283220e2994a1fae56
Fayrr <penguin-commander@hotmail.com>
2014-05-16 14:29:01 +1200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
+# Class defines a user 
 class User < ActiveRecord::Base
 	validates :username, :presence => true, :uniqueness => true
 	validates :email, :presence => true, :uniqueness => true;
 	validates_confirmation_of :password
 	validates_length_of :password, :in => 6..20, :on => :create
 	
+	# Authenticate a users log in attempt by comparing their username and password against the database
 	def self.authenticate(login_username="", login_password="")
+		# Grab the user
 		user = User.find_by_username(login_username)
+		# If the user exists and passwords match
 		if user && user.password == login_password
+			# Return that user
 			return user
+		# Else
 		else 
+			# Return false to invalidate the login
 			return false
 		end
 	end
