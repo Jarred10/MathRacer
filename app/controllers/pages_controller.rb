a1439cfdcf93b41863386a7d451903d245d1b9ab
aoteasila <wk24@waikato.ac.nz>
2014-05-16 14:36:42 +1200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# This class control the page, compare user data with a database and only an authenticate user can play. 
+# This play login and register links after the user logged out
 class PagesController < ApplicationController
 
 	before_filter :authenticate_user, :only => [:play]
